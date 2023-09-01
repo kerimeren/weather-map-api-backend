3a9ec4f43fd945ef05d00fad24478bc91d5fd2d1
kerimeren <kerimeren17@gmail.com>
2023-09-02 01:16:16 +0300

    Initial commit

---
+++ src/main/java/com/example/weathermapapi/WeatherMapApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.weathermapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weathermapapi/WeatherMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weathermapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
